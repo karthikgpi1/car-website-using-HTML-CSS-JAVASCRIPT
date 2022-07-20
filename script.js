ab5672583aca21ea04ea8df4983293c9dd066e4f
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-20 19:24:58 +0530

    user login

---
+++ script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classlist.add('active');
+    }else{
+        document.querySelector('.header').classlist.remove('active');
+    }
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classlist.add('active');
+    }else{
+        document.querySelector('.header').classlist.remove('active');
+    }
+
+  
+}
+
