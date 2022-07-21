5524c4fc40b6b3b804f1c1d7c599db8b9cdf9e1d
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-21 08:29:45 +0530

    icon sections in HTML

--- script.js
+++ script.js
@@ -39,15 +39,10 @@ window.onload = () => {
 
 }
 
-document.querySelector('.home').onmousemove = (e) => {
+document.querySelector('.home').onmouseleave = () => {
 
     document.querySelectorAll('.home-parallax').forEach(elm => {
 
-        let speed = elm.getAttribute('data-speed');
-
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
-
-        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+        elm.style.transform = `translateX(0px) translateY(0px)`;
     });
 };
