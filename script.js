426e425cd74449f74988fc91342cc6c10b88b157
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-21 11:46:17 +0530

    vehicle slider in js

--- script.js
+++ script.js
@@ -59,3 +59,35 @@ document.querySelector('.home').onmouseleave = () => {
         elm.style.transform = `translateX(0px) translateY(0px)`;
     });
 };
+
+//vehicle slider------
+
+var swiper = new Swiper(".vehicles-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      }, 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        // spaceBetween: 40,
+      },
+      991: {
+        slidesPerView: 3,
+        // spaceBetween: 50,
+      },
+    },
+});
+
+//vehicle slider end---
