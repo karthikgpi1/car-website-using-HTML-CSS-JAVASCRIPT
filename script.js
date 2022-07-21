5a16f23622c514bf9772cb77f52947d8655a0fdc
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-21 17:14:30 +0530

    reviews section in javascript

--- script.js
+++ script.js
@@ -128,3 +128,35 @@ var swiper = new Swiper(".featured-slider", {
 });
 
 //featured slider end--
+
+// reviews section slider starts:
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop:true,
+  grabCursor:true,
+  centeredSlides:true,
+  autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    }, 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      // spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      // spaceBetween: 40,
+    },
+    1045: {
+      slidesPerView: 3,
+      // spaceBetween: 50,
+    },
+  },
+});
+// reviews section slider endss:
