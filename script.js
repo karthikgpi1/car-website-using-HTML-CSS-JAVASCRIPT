61f3e58689a91475f13e2ec18446f2bdf76e34ea
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-21 09:18:09 +0530

    icons section in CSS

--- script.js
+++ script.js
@@ -39,6 +39,19 @@ window.onload = () => {
 
 }
 
+document.querySelector('.home').onmousemove = (e) => {
+
+    document.querySelectorAll('.home-parallax').forEach(elm => {
+
+        let speed = elm.getAttribute('data-speed')
+        
+        let x = (window.innerWidth - e.pageX * speed) / 90;
+        let y = (window.innerHeight - e.pageY * speed) / 90;
+
+        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+    });
+};
+
 document.querySelector('.home').onmouseleave = () => {
 
     document.querySelectorAll('.home-parallax').forEach(elm => {
